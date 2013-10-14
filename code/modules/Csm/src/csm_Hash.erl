aef15c65621ba62426591bbeb70b160b310969a9
Sebastian Weddmark Olsson <sebastiw@student.chalmers.se>
2013-10-14 09:25:22 +0200

    added function to csm_Hash

--- code/modules/Csm/src/csm_Hash.erl
+++ code/modules/Csm/src/csm_Hash.erl
@@ -8,3 +8,12 @@
 -include("inc/memMap.hrl").
 %-include("inc/det.hrl"). % OPTIONAL
 -include("../Cry/inc/cry_Hash.hrl").
+
+csm_HashStart(CfgId) when IsStarted == true ->
+  csm_E_NOT_OK;
+csm_HashStart(CfgId) when IsActive == false ->
+  IsStarted = true,
+  IsActive = true,
+  csm_E_OK;
+csm_HashStart(CfgId) ->
+  csm_E_BUSY.
