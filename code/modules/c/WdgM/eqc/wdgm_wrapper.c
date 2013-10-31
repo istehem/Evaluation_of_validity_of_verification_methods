800911f5ef64d369ee222c010409d6b3180d89d1
Sebastian Weddmark Olsson <sebastiw@student.chalmers.se>
2013-10-31 11:22:18 +0100

    planning report version 2

--- code/modules/c/WdgM/eqc/wdgm_wrapper.c
+++ code/modules/c/WdgM/eqc/wdgm_wrapper.c
@@ -1,4 +1,5 @@
 #include "h_files/WdgM.h"
 
-//eqc_c:start(tst,[{c_src,"eqc/tst.c"},{include_functions,["WdgM_Init"]},{additional_files,["out/WdgM_Pbcfg.o","out/WdgM_Lcfg.o","out/Dem.o","out/Det.o","out/Mcu.o","out/Rte.o","out/SchM_WdgM.o","out/WdgIf.o"]}]) .
+const WdgM_ConfigType * Tst_Cfg1_ptr = &Tst_Cfg1;
 
+//eqc_c:start(tst,[{c_src,"eqc/tst.c"},{include_functions,["WdgM_Init"]},{additional_files,["out/WdgM_Pbcfg.o","out/WdgM_Lcfg.o","out/Dem.o","out/Det.o","out/Mcu.o","out/Rte.o","out/SchM_WdgM.o","out/WdgIf.o"]}]) .
