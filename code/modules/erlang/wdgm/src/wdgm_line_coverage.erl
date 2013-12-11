ceaa841902b02a3ff4de2ac58fa9ad73e9c59e81
Oskar Ingemarsson <oskar.ingemarsson@mecel.se>
2013-12-11 15:25:57 +0000

    now seems to be working

--- code/modules/erlang/wdgm/src/wdgm_line_coverage.erl
+++ code/modules/erlang/wdgm/src/wdgm_line_coverage.erl
@@ -16,20 +16,32 @@ modules() ->
     %wdgm_command, %% Seems not to be running, probable cause "only seen in generation step"
   ].
 
-write_runsuits_and_analyse(_,[]) ->
+write_runsuits_and_analyse(_,[],_) ->
   ok;
-write_runsuits_and_analyse(S,[X|Xs]) ->
-  eqc_suite:run(wdgm_statem_eqc:prop_wdgm_init(),X),
+write_runsuits_and_analyse(S,[X|Xs],Opts) ->
   eqc_suite:write(X,S),
-  cover:analyse_to_file(X),
-  write_runsuits_and_analyse(S,Xs).
+  cover:reset(X),
+  eqc_suite:run(wdgm_statem_eqc:prop_wdgm_init(),X),
+  case Opts of
+    false -> cover:analyse_to_file(X);
+    _     -> cover:analyse_to_file(X,Opts)
+  end,
+  write_runsuits_and_analyse(S,Xs,Opts).
 
-run_coverage(N) ->
+run_coverage(N,Opts) ->
   compile(),
   S = eqc_suite:feature_based(
     eqc_suite:line_coverage(modules(),
       eqc:numtests(N,
         wdgm_statem_eqc:prop_wdgm_init()))),
-  write_runsuits_and_analyse(S,modules()),
-  cover:reset().
+  write_runsuits_and_analyse(S,modules(),Opts).
+
+generate_coverage_report(N) ->
+  run_coverage(N,false).
+
+generate_coverage_report(N,_Opts) ->
+  run_coverage(N,[html]).
+
+
+
 
