000ff6ff59818abc4e423e1a1335f8e064a4ccc9
Oskar Ingemarsson <oskar.ingemarsson@mecel.se>
2013-12-19 14:24:27 +0000

    now working without using crapy suite

--- code/modules/erlang/wdgm/src/wdgm_line_coverage.erl
+++ code/modules/erlang/wdgm/src/wdgm_line_coverage.erl
@@ -4,7 +4,6 @@
 compile() ->
   cover:compile_directory("../src").
 
-
 modules() ->
   [
     wdgm_next,
@@ -16,25 +15,22 @@ modules() ->
     %wdgm_command, %% Seems not to be running, probable cause "only seen in generation step"
   ].
 
-write_runsuits_and_analyse(_,[],_) ->
+analyse(_,[],_) ->
   ok;
-write_runsuits_and_analyse(S,[X|Xs],Opts) ->
-  eqc_suite:write(X,S),
-  cover:reset(X),
-  eqc_suite:run(wdgm_statem_eqc:prop_wdgm_init(),X),
+analyse(N,[X|Xs],Opts) ->
   case Opts of
-    false -> cover:analyse_to_file(X);
-    _     -> cover:analyse_to_file(X,Opts)
+    false -> %cover:analyse(X,coverage,line),
+             cover:analyse_to_file(X);
+    _     -> %cover:analyse(X,coverage,line)
+             cover:analyse_to_file(X,Opts)
   end,
-  write_runsuits_and_analyse(S,Xs,Opts).
+  cover:reset(X),
+  analyse(N,Xs,Opts).
 
 run_coverage(N,Opts) ->
   compile(),
-  S = eqc_suite:feature_based(
-    eqc_suite:line_coverage(modules(),
-      eqc:numtests(N,
-        wdgm_statem_eqc:prop_wdgm_init()))),
-  write_runsuits_and_analyse(S,modules(),Opts).
+  eqc:quickcheck(eqc:numtests(N,wdgm_statem_eqc:prop_wdgm_init())),
+  analyse(N,modules(),Opts).
 
 generate_coverage_report(N) ->
   run_coverage(N,false).
