d58cc09201872f7eedce8e7462ae0962eb0f642f
Oskar Ingemarsson <oskari@student.chalmers.se>
2014-01-21 16:34:55 +0100

    some auto stuff

--- code/modules/plotting/status_per_length.sh
+++ code/modules/plotting/status_per_length.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [ $1 ] && [ -f $1 ]; then 
-    SCRIPT_PATH=$(dirname $0)
+SCRIPT_PATH=$(dirname $0)
+
+if [ $1 ] && [ -f $1 ]; then
+    if [ ! -d $SCRIPT_PATH/pdf ]; then
+          mkdir $SCRIPT_PATH/pdf
+    fi  
     echo $(pwd)/$1 | python $SCRIPT_PATH/bin/status_per_length.py --format=pdf > $SCRIPT_PATH/pdf/status_per_length.pdf
 else
     echo "usage: $0 path_to_history_file"
