bd5b379f8df270829945e1455b8e3349ccdb44be
Oskar Ingemarsson <oskar.ingemarsson@mecel.se>
2014-02-25 14:30:49 +0000

    prints stats in linecoverage extra target added in Makefile

--- code/modules/erlang/wdgm/src/wdgm_line_coverage.erl
+++ code/modules/erlang/wdgm/src/wdgm_line_coverage.erl
@@ -2,7 +2,11 @@
 -compile(export_all).
 
 compile() ->
-  lists:map(fun(X) -> cover:compile_module("../src/" ++ atom_to_list(X)) end,modules()).
+  lists:map(fun(X) -> case cover:is_compiled(X) of
+                          false -> cover:compile_module("../src/" ++ atom_to_list(X),[debug_info]);
+                          _     -> ok
+                      end
+                    end,modules()).
 
 modules() ->
   [
@@ -20,16 +24,28 @@ analyse([],_) ->
 analyse([X|Xs],Opts) ->
   File = atom_to_list(X) ++ ".coverdata",
   case file:read_file_info(File) of
-    {ok,_} -> cover:import(File);
+    {ok,_} -> case lists:member(X,cover:imported_modules()) of
+                true -> ok;
+                _    -> cover:import(File)
+              end;
     _      -> ok
   end,
   case Opts of
-    false -> %cover:analyse(X,coverage,line),
+    false -> {_,Ans} = cover:analyse(X,coverage,line),
+             io:fwrite("line_coverage=~p\n",[return_cover_stats(Ans,0,length(Ans),X)]),
              cover:analyse_to_file(X);
-    _     -> %cover:analyse(X,coverage,line)
+    _     -> {_,Ans} = cover:analyse(X,coverage,line),
+             io:fwrite("line_coverage=~p\n",[return_cover_stats(Ans,0,length(Ans),X)]),
              cover:analyse_to_file(X,Opts)
   end,
   cover:export(File,X),
   cover:reset(X),
   analyse(Xs,Opts).
 
+return_cover_stats([],N,L,M) ->
+  "Coverage for module " ++ atom_to_list(M) ++ "=" ++ float_to_list((N/L)*100) ++ "%";
+return_cover_stats([A|Ans],N,L,M) ->
+  case A of
+    {{M,_},{1,_}} -> return_cover_stats(Ans,N + 1,L,M);
+    _             -> return_cover_stats(Ans,N,L,M)
+  end.
